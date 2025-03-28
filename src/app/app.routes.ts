4aabda1ca0d912b79bc1a96fa91afad9613b101b
jonas <jonas@rotetobben.de>
2025-03-28 17:08:24 +0100

    Navigation hinzugefügt und Universal Styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AktuellesComponent } from './pages/aktuelles/aktuelles.component';
+import { TrainingComponent } from './pages/training/training.component';
+import { FomulareComponent } from './pages/fomulare/fomulare.component';
+import { KontaktComponent } from './pages/kontakt/kontakt.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'Startseite - ACD' },
+    { path: 'aktuelles', component: AktuellesComponent, title: 'Aktuelles - ACD' },
+    { path: 'training', component: TrainingComponent, title: 'Training - ACD' },
+    { path: 'fomulare', component: FomulareComponent, title: 'Fomulare - ACD' },
+    { path: 'kontakt', component: KontaktComponent, title: 'Kontakt - ACD' }
+];
---
+++ src/app/pages/aktuelles/aktuelles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aktuelles',
+  standalone: true,
+  imports: [],
+  templateUrl: './aktuelles.component.html',
+  styleUrl: './aktuelles.component.scss'
+})
+export class AktuellesComponent {
+
+}
