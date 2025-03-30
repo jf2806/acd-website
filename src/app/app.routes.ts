13401ce4494efa27cf0236fd870c4ae99f5653d4
jonas <jonas@rotetobben.de>
2025-03-30 17:06:26 +0200

    footer hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { AktuellesComponent } from './pages/aktuelles/aktuelles.component';
 import { TrainingComponent } from './pages/training/training.component';
 import { FomulareComponent } from './pages/fomulare/fomulare.component';
 import { KontaktComponent } from './pages/kontakt/kontakt.component';
+import { DatenschutzComponent } from './pages/datenschutz/datenschutz.component';
+import { ImpressumComponent } from './pages/impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, title: 'Startseite - ACD' },
     { path: 'aktuelles', component: AktuellesComponent, title: 'Aktuelles - ACD' },
     { path: 'training', component: TrainingComponent, title: 'Training - ACD' },
     { path: 'fomulare', component: FomulareComponent, title: 'Fomulare - ACD' },
-    { path: 'kontakt', component: KontaktComponent, title: 'Kontakt - ACD' }
+    { path: 'kontakt', component: KontaktComponent, title: 'Kontakt - ACD' },
+    { path: 'datenschutz', component: DatenschutzComponent, title: 'Datenschutz - ACD' },
+    { path: 'impressum', component: ImpressumComponent, title: 'Impressum - ACD' }
 ];
